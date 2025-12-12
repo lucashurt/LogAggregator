0c7c7476afc40a627e79b51f0eccc8470c68625d
lucashurt <lucashurtado814@gmail.com>
2025-12-12 13:15:23 -0500

    created log entry model and status ENUM

---
+++ backend/src/main/java/com/example/logaggregator/logs/LogEntry.java
@@ -0,0 +1,45 @@
+package com.example.logaggregator.logs;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.time.Instant;
+import java.util.Map;
+
+@Entity
+@Table(name = "log_entries", indexes = {
+        @Index(name = "idx_timestamp", columnList = "timestamp"),
+        @Index(name = "idx_serviceId", columnList = "serviceId"),
+        @Index(name = "idx_status", columnList = "status")
+})
+@Data
+public class LogEntry {
+    @Id  // <-- ADD THIS
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Instant timestamp;
+
+    @Column(nullable = false)
+    private int serviceId;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private LogStatus status;
+
+    @Column(nullable = false)
+    private String message;
+
+    @Column(columnDefinition = "JSONB")
+    @JdbcTypeCode(SqlTypes.JSON)  // Use this instead of @Type
+    private Map<String,Object> metadata;
+
+    @Column(unique = true)
+    private String traceId;
+
+    @Column(nullable = false)
+    private Instant Created;
+}
---
+++ backend/src/main/java/com/example/logaggregator/logs/LogStatus.java
@@ -0,0 +1,8 @@
+package com.example.logaggregator.logs;
+
+public enum LogStatus {
+    Info,
+    Debug,
+    Warning,
+    Error
+}
