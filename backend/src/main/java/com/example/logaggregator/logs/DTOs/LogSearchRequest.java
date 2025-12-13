dcdae3311bd43779cecf1b50e949dbb76ca10e46
lucashurt <lucashurtado814@gmail.com>
2025-12-13 13:06:34 -0500

    created search specification for dynamic queries

--- backend/src/main/java/com/example/logaggregator/logs/DTOs/LogSearchRequest.java
+++ backend/src/main/java/com/example/logaggregator/logs/DTOs/LogSearchRequest.java
@@ -4,5 +4,9 @@ import com.example.logaggregator.logs.models.LogStatus;
 
 import java.time.Instant;
 
-public record LogSearchRequest(String serviceId, LogStatus level, String traceId, Instant startTimestamp, Instant endTimestam, String query, Integer page, Integer size) {
+public record LogSearchRequest(String serviceId, LogStatus level, String traceId, Instant startTimestamp, Instant endTimestamp, String query, Integer page, Integer size) {
+    public LogSearchRequest{
+        page = (page != null && page >= 0) ? page : 0;
+        size = (size != null && size > 0 && size <=1000) ? size : 50;
+    }
 }
---
+++ backend/src/main/java/com/example/logaggregator/logs/LogEntrySpecification.java
@@ -0,0 +1,48 @@
+package com.example.logaggregator.logs;
+
+import com.example.logaggregator.logs.DTOs.LogSearchRequest;
+import com.example.logaggregator.logs.models.LogEntry;
+import org.springframework.data.jpa.domain.Specification;
+import jakarta.persistence.criteria.Predicate;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class LogEntrySpecification {
+    public Specification<LogEntry> buildSpecification(LogSearchRequest request){
+        return (root, criteriaQuery, criteriaBuilder) -> {
+            List<Predicate> predicates = new ArrayList<>();
+
+            if(request.serviceId() != null && !request.serviceId().isBlank()){
+                predicates.add(criteriaBuilder.equal(root.get("serviceId"),request.serviceId()));
+            }
+
+            if(request.traceId() != null && !request.traceId().isBlank()){
+                predicates.add(criteriaBuilder.equal(root.get("traceId"),request.traceId()));
+            }
+
+            if(request.level() != null){
+                predicates.add(criteriaBuilder.equal(root.get("level"),request.level()));
+            }
+
+            if (request.startTimestamp() != null && request.endTimestamp() != null && request.startTimestamp().isBefore(request.endTimestamp())) {
+                predicates.add(criteriaBuilder.between(
+                        root.get("timestamp"),
+                        request.startTimestamp(),
+                        request.endTimestamp()
+                ));
+            }
+
+            if(request.query() != null && !request.query().isBlank()){
+                predicates.add(criteriaBuilder.like(
+                        criteriaBuilder.lower(root.get("message")),
+                        "%" + request.query().toLowerCase() + "%"
+                ));
+            }
+
+            return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+        };
+    }
+}
